3f37602662ab8d47b27a9edd5642f2efce3252ea
colazeus <xiaoxuan199@gmail.com>
2014-01-12 16:44:32 +0800

    Initial Commit

---
+++ Yande/YandeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YandeAppDelegate.h
+//  Yande
+//
+//  Created by 南 篤良 on 14-1-12.
+//  Copyright (c) 2014年 南 篤良. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YandeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Yande/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Yande
+//
+//  Created by 南 篤良 on 14-1-12.
+//  Copyright (c) 2014年 南 篤良. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YandeAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YandeAppDelegate class]));
+    }
+}
